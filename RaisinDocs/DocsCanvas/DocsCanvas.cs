3a6af63949eace459cec930880f809e8394a712d
gerleim <gerleim@gmail.com>
2026-08-02 23:58:46 +0200

    Remove unused usings from DocsCanvas.cs
    - Removed: using System.Windows.Controls
    - Removed: using System.Windows.Controls.Primitives
    - Build verified: 0 errors
    - Retained 6 actively used usings

--- RaisinDocs/DocsCanvas/DocsCanvas.cs
+++ RaisinDocs/DocsCanvas/DocsCanvas.cs
@@ -1,7 +1,5 @@
 using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
